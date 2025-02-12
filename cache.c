08cc4fc82b32788df595a02b6f2ca54eaec528d1
Nolan Cyr <nolangcyr@gmail.com>
2025-02-12 14:19:48 -0500

    create c file

---
+++ cache.c
@@ -0,0 +1,10 @@
+// PA2 Simulation of a Hardware Cache
+// Authors: Nolan Cyr and Eric Edlund
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
